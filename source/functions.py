b1821e095be4df49967d397d50c500b4f4d534df
davideguerra <guerradavide9@gmail.com>
2020-04-24 00:34:25 +0200

    added nozzle functions

--- source/functions.py
+++ source/functions.py
@@ -2,6 +2,19 @@ import numpy as np
 from scipy.optimize import fsolve
 from rocketcea.cea_obj_w_units import CEA_Obj
 
+#GENERAL FUNCTIONS
+
+def calcDiameter(A):
+    return np.sqrt(4*A/np.pi)
+
+def calcVolume(M, rho):
+    return M/rho
+
+def calcDensity(p, R, T):
+    return p/(R*T)
+
+#PROPULSION PARAMETERS
+
 def calcGasConstant(Ru,MM):
     return Ru*1e3/MM;
 
@@ -38,9 +51,6 @@ def calcPropFlowRate(T_z, u_e, eps, p_c, ct, p_e, p_z):
 def calcThroatArea(m_p, u_e, p_c, ct):
     return (m_p*u_e)/(p_c*ct)
 
-def calcDiameter(A):
-    return np.sqrt(4*A/np.pi)
-
 def calcExitArea(A_t,eps):
     return A_t*eps
 
@@ -71,9 +81,6 @@ def calcOxidizerRate(m_f, r):
 def calcMass(m, t_b, k_s):
     return m*t_b*k_s
 
-def calcVolume(M, rho):
-    return M/rho
-
 def calcRhoAvg(M_f, M_ox, V_f, V_ox):
     return (M_f+M_ox)/(V_f+V_ox)
 
@@ -86,11 +93,10 @@ def calcVolumetricSpecificImpulse(I_s, rho_avg):
 def calcTotalVolumetricImpulse(I_s, M_ox, M_f, g_0):
     return I_s*(M_ox+M_f)*g_0
 
+#COMBUSTION CHAMBER
+
 def calcChamberVelocity(Ma_c, y_c, R_c, T_c):
     return Ma_c*np.sqrt(y_c*R_c*T_c)
-
-def calcDensity(p, R, T):
-    return p/(R*T)
 
 def calcChamberArea(m_p, rho_c, u_c):
     return m_p/(rho_c*u_c)
@@ -100,6 +106,8 @@ def calcChamberVolume(L_star, A_c):
 
 def calcChamberLength(V_c, A_c):
     return V_c/A_c
+
+#INJECTOR PLATE
 
 def calcTotalHolesArea(m, C_d, rho, p_loss_inj, p_c):
     return m/(C_d*np.sqrt(2*rho*p_loss_inj*p_c))
@@ -118,11 +126,62 @@ def calcInjectionVelocity(C_d, p_loss_inj, p_c, rho):
 
 def calcFuelInjectionAngle(m_ox, m_f, u_ox, u_f, alfa_ox):
     return np.arcsin(np.sin(alfa_ox)*(m_ox*u_ox)/(m_f*u_f))
+
 def calcPStarF(p_c,P_loss_inj,P_loss_exc,P_loss_feed,P_loss_valves):
     return p_c*(1-P_loss_inj-P_loss_exc-P_loss_feed-P_loss_valves)
 
+#FEED LINE
+
 def calcPStarOx(p_c,P_loss_inj,P_loss_feed,P_loss_valves):
     return p_c*(1-P_loss_inj-P_loss_feed-P_loss_valves)
+
+#NOZZLE
+
+def calcConeLength(eps, D_t, theta):
+    return (np.sqrt(eps)-1)*(D_t/2)/np.tan(theta)
+
+def calcBellLength(L_cone):
+    return 0.8*L_cone
+
+def calcThetai(D_c, D_t):
+    f = lambda x: D_c/2-1.5*(D_t/2)*np.sin(x)-1.5*(D_t/2)-D_t/2
+    return fsolve(f, (-3/4)*np.pi)#VALORE TIPICO theta_i
+
+def calcXConv(theta_i, D_t):
+    interval = np.arange(theta_i, -np.pi/2, 0.01)
+    return 1.5*(D_t/2)*np.cos(interval)
+
+def calcYConv(theta_i, D_t):
+    interval = np.arange(theta_i, -np.pi/2, 0.01)
+    return 0.382*(D_t/2)*(np.sin(interval)+1)+(D_t/2)
+
+def calcXDivPlus(theta_n, D_t):
+    interval = np.arange(-np.pi/2, theta_n-np.pi/2, 0.01)
+    return 0.382*(D_t/2)*np.cos(interval)
+
+def calcXDivPlus(theta_n, D_t):
+    interval = np.arange(-np.pi/2, theta_n-np.pi/2, 0.01)
+    return 0.382*(D_t/2)*(np.sin(interval)+1)+D_t/2
+
+def calcXDivMinus(theta_n, theta_e, D_t, L_bell, D_e):
+    N_x = 0.382*(D_t/2)*np.cos(theta_n-np.pi/2)
+    N_y = 0.382*(D_t/2)*(np.sin(theta_n-np.pi/2)+1)+D_t/2
+    C_1 = N_y-np.tan(theta_n)*N_x
+    C_2 = (D_e/2)-np.tan(theta_e)*L_bell
+    Q_x = (C_2-C_1)/(np.tan(theta_n)-np.tan(theta_e))
+    t = np.arange(0, 1, 0.01)
+    return N_x*(1-t)**2 + 2*Q_x*(1-t)*t + L_bell*t**2
+
+def calcYDivMinus(theta_n, theta_e, D_t, L_bell, D_e):
+    N_x = 0.382 * (D_t / 2) * np.cos(theta_n - np.pi / 2)
+    N_y = 0.382 * (D_t / 2) * (np.sin(theta_n - np.pi / 2) + 1) + D_t / 2
+    C_1 = N_y - np.tan(theta_n) * N_x
+    C_2 = (D_e / 2) - np.tan(theta_e) * L_bell
+    Q_y = (np.tan(theta_n)*C_2-np.tan(theta_e)*C_1)/(np.tan(theta_n)-np.tan(theta_e))
+    t = np.arange(0, 1, 0.01)
+    return N_y*(1-t)**2 + 2*Q_y*(1-t)*t + (D_e/2)*t**2
+
+#CEA
 
 def getCeaObj(fuelName, oxName):
     #print(type(fuelName),type(oxName))
