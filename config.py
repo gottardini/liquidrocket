d272df703ea418e55cd99a19b6b921f699bf3a55
gottardini <danielegott98@gmail.com>
2020-04-24 15:51:48 +0200

    Implemented basic scheme for all-stages models

--- config.py
+++ config.py
@@ -1,3 +1,17 @@
 jobfile="source/job.txt"
 datafile="source/data.csv"
 postprocfile="source/postproc.txt"
+
+
+###ROCKETS SETTINGS
+rockets={
+    "Ariane 5 ECA":[
+        {
+        'name':"Vulcain 2",
+        'index':0,
+        'qty':1,
+        'tStart':0,
+        'tEnd':100
+        },
+    ]
+}
