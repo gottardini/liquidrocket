1e782590a3e7ccf2dbb828e588ecb77a16b793d7
gottardini <danielegott98@gmail.com>
2020-04-29 11:02:38 +0200

    Basic version of solid boosters

--- source/solid.py
+++ source/solid.py
@@ -3,3 +3,7 @@ from source.functions import *
 import numpy as np
 
 ###TODO
+prop={
+    'thr_n':UnknownVariable("Spinta nominale",'N',CalcFunction(kiloNewtonToNewton,'T_n')),
+    'thr_var':UnknownVariable("Spinta in funzione della quota",'N',CalcFunction(lambda z,t: np.full(len(z),t), 'z','thr_n'))
+}
