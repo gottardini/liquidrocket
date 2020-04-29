751acbb07131ff91b6b31ccc3f67a66ee9c783e2
gottardini <danielegott98@gmail.com>
2020-04-29 18:12:37 +0200

    Added Isp plots

--- postprocessing/__init__.py
+++ postprocessing/__init__.py
@@ -35,17 +35,26 @@ class PostProcesser:
 
     ###THIS IS ABOUT TO GET MESSY AND NOT FLEXIBLE, BUT I HAVE NO TIME TO MAKE IT BTFL
     def make(self):
-        unified={}
         ###THRUST-Z
         figThrust_z, axThrust_z = plt.subplots(figsize=(8, 8),num="Spinta in funzione della quota")
         axThrust_z.set_xlabel("Quota [m]")
         axThrust_z.set_ylabel("Spinta [N]")
+
+        ###ISP-Z
+        figIsp_z, axIsp_z = plt.subplots(figsize=(8, 8),num="Impulso specifico in funzione della quota")
+        axIsp_z.set_xlabel("Quota [m]")
+        axIsp_z.set_ylabel("Impulso specifico [s]")
 
         for rocketName, rocket in self.rockets.items():
             ###THRUST-Z
             sep_figThrust_z, sep_axThrust_z = plt.subplots(figsize=(8, 8),num="Spinta in funzione della quota - %s"%(rocketName))
             sep_axThrust_z.set_xlabel("Quota [m]")
             sep_axThrust_z.set_ylabel("Spinta [N]")
+
+            ###ISP-Z
+            sep_figIsp_z, sep_axIsp_z = plt.subplots(figsize=(8, 8),num="Impulso specifico in funzione della quota- %s"%(rocketName))
+            sep_axIsp_z.set_xlabel("Quota [m]")
+            sep_axIsp_z.set_ylabel("Impulso specifico [s]")
 
             for blockIndex in range(len(rocket)):
                 block=rocket[blockIndex]
@@ -57,3 +66,10 @@ class PostProcesser:
                 sep_axThrust_z.set_yscale('log')
                 sep_axThrust_z.grid(True, which="both")
                 sep_axThrust_z.set_title("Contributo spinta dei vari stadi - "+rocketName)
+
+                ###THRUST-Z
+                sep_axIsp_z.plot(blockSolved['z'].getValue(),blockSolved['I_s'].getValue(),label=block['name'])
+                sep_axIsp_z.legend()
+                sep_axIsp_z.set_yscale('linear')
+                sep_axIsp_z.grid(True, which="both")
+                sep_axIsp_z.set_title("Impulso specifico dei vari stadi - "+rocketName)
