c27cc11c41d318f20b3d701e8f8c030a577780c4
gottardini <danielegott98@gmail.com>
2020-04-30 10:51:40 +0200

    Added some documentation

--- latexer/__init__.py
+++ latexer/__init__.py
@@ -1,3 +1,32 @@
+import numpy as np
+
+"""
+"rockets" object is a dictionary of lists:
+{
+"Ariane V ECA": [...],
+"Falcon Heavy": [...],
+"Saturn V": [...]
+}
+The exact key names are defined in config.py
+
+Each list is a list of "blocks", where a "block" is a group of engines of the same kind working within the same timespan
+
+Each block is a dictionary defined as follows:
+{
+'name': "<stage name>",
+'index': <engine index>,
+'qty': <engines quantity>,
+'type': '<engine type>',
+'tStart': <ignition time>,
+'tEnd': <cut-off time>,
+'zStart': <ignition altitude>,
+'zEnd': <cut-off altitude>,
+'solvedData': <data dictionary of the solved model>
+}
+
+The "solved data dictionary" is just the big dictionary with every imaginable variable of the model
+
+"""
 
 class Latexer:
     def __init__(self,rockets):
