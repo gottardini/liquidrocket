2a402b8b34497425929a6af34964e7c7ea878e4f
davideguerra <guerradavide9@gmail.com>
2020-05-01 19:46:18 +0200

    Latex table generator created

--- latexer/__init__.py
+++ latexer/__init__.py
@@ -30,13 +30,46 @@ The "solved data dictionary" is just the big dictionary with every imaginable va
 """
 
 tables={
-    "Parametri propulsivi":(['Is_vac','u_e'],["Vulcain II","HM7B","Merlin 1D+","Merlin 1D+ vac","F-1","J-2 (II° stadio)","J-2 (III° stadio)"])
+    "Parametri propulsivi":(['Is_vac','u_e'],["Vulcain II","HM7B","Merlin 1D+","Merlin 1D+ vac","F-1","J-2 (II° stadio)"])
 }
-
+aliases={
+    "J-2 (II° stadio)":"J-2"
+}
 
 class Latexer:
     def __init__(self,rockets):
         self.rockets=rockets
 
     def make(self):
-        pass
+        output = ""
+        engines = {}
+        for rocketName, rocketData in self.rockets.items():
+            for block in rocketData:
+                if block['engName'] not in engines:
+                    engines[block['engName']]=block['solvedData']
+
+        for tableName,tableValues in tables.items():
+            names = []
+            for name in tableValues[1]:
+                if name in aliases:
+                    names.append(aliases[name])
+                else:
+                    names.append(name)
+            output += (tableName.upper() +  "\n\n"
+            "\\begin{center}"
+            "\\begin{tabular}{*{"+ str(len(tableValues[1])+2) +"}{|c}|} \n"
+            "\\hline \n"
+            "\\multicolumn{"+ str(len(tableValues[1])+2) +"}{|c|}{"+ tableName +"} \\\\ \n"
+            "\\hline \n"
+            "Variabile & Unità di misura & " + ' & '.join(names) +"\\\\ \n")
+            for i in np.arange(len(tableValues[0])):
+                values = [list(engines.items())[0][1][tableValues[0][i]].units]
+                values += ['{:.2f}'.format(engines[engName][tableValues[0][i]].getValue()[0]) for engName in tableValues[1]]
+                output += "\\hline \n$" + tableValues[0][i].replace('_', '\_') + '$ & ' + ' & '.join(values) + "\\\\ \n"
+            output += (" \\hline \n"
+            "\\end{tabular}"
+            "\\end{center} \n\n")
+
+        f = open("out/latex_tables.txt", 'w+')
+        f.write(output)
+        f.close()
