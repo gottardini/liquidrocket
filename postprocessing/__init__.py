4a9882fc5fe1e9d8cca2020eb60d1683a1f37f24
gottardini <danielegott98@gmail.com>
2020-05-03 16:09:10 +0200

    Refactor of postprocessing module + implemented thrust coefficent plot

--- postprocessing/__init__.py
+++ postprocessing/__init__.py
@@ -6,70 +6,96 @@ class PostProcesser:
     def __init__(self,rockets):
         self.rockets=rockets
 
-        """
-        ###GRAFICI
-        self.figures=OrderedDict({
-            "Pressione in funzione della quota":['z',['p','p_tropo','p_e']],
-            "Spinta in funzione della pressione":['p',['thr_var','thr_n','thr_ad']],
-            "Spinta in funzione della quota":['z',['thr_var','thr_n','thr_ad']],
-            })
+        self.rocketColors=['#cc3300','#009900','#003399']
+        self.stageColors=['#6600cc','#997300','#008060','black']
+        self.stageStyles=['-','--','-.',':']
 
-        self.styles=[
-            [{"color":"black"},{"color":"gray"},{"color":"green","linestyle":"--"}],
-            [{"color":"blue"},{"color":"gray","linestyle":"--"},{"color":"green","linestyle":"--"}],
-            [{"color":"blue"},{"color":"gray","linestyle":"--"},{"color":"green","linestyle":"--"}],
-        ]
-
-        self.markers=[
-            [{"data":"z_ad","params":{"color":"gray","linestyle":"--","linewidth":1}}],
-            [{"data":"p_e","params":{"color":"gray","linestyle":"--","linewidth":1}}],
-            [{"data":"z_ad","params":{"color":"gray","linestyle":"--","linewidth":1}}],
-        ]
-
-        self.labels=[
-            ("Quota [m]","Pressione [Pa]"),
-            ("Pressione [Pa]","Spinta [N]"),
-            ("Quota [m]","Spinta [N]"),
-        ]
-        """
 
     ###THIS IS ABOUT TO GET MESSY AND NOT FLEXIBLE, BUT I HAVE NO TIME TO MAKE IT BTFL
     def make(self):
         ###THRUST-Z
-        figThrust_z, axThrust_z = plt.subplots(figsize=(8, 8),num="Spinta in funzione della quota")
+        figThrust_z, axThrust_z = plt.subplots(figsize=(12, 8),num="Spinta in funzione della quota")
         axThrust_z.set_xlabel("Quota [m]")
         axThrust_z.set_ylabel("Spinta [N]")
+        axThrust_z.set_title("Spinta in funzione della quota")
+        axThrust_z.set_yscale('log')
+        axThrust_z.grid(True, which="both")
 
         ###ISP-Z
-        figIsp_z, axIsp_z = plt.subplots(figsize=(8, 8),num="Impulso specifico in funzione della quota")
+        figIsp_z, axIsp_z = plt.subplots(figsize=(12, 8),num="Impulso specifico in funzione della quota")
         axIsp_z.set_xlabel("Quota [m]")
         axIsp_z.set_ylabel("Impulso specifico [s]")
+        axIsp_z.set_title("Impulso specifico in funzione della quota")
+        axIsp_z.set_yscale('linear')
+        axIsp_z.grid(True, which="both")
 
+        ###CT-Z
+        figCt_z, axCt_z = plt.subplots(figsize=(12, 8),num="Coefficiente di spinta in funzione della quota")
+        axCt_z.set_xlabel("Quota [m]")
+        axCt_z.set_ylabel("Coefficiente di spinta [-]")
+        axCt_z.set_title("Coefficiente di spinta in funzione della quota")
+        axCt_z.set_yscale('linear')
+        axCt_z.grid(True, which="both")
+
+        i=0
         for rocketName, rocket in self.rockets.items():
-            ###THRUST-Z
-            sep_figThrust_z, sep_axThrust_z = plt.subplots(figsize=(8, 8),num="Spinta in funzione della quota - %s"%(rocketName))
+            ###SEP THRUST-Z
+            sep_figThrust_z, sep_axThrust_z = plt.subplots(figsize=(12, 8),num="Spinta in funzione della quota - %s"%(rocketName))
             sep_axThrust_z.set_xlabel("Quota [m]")
             sep_axThrust_z.set_ylabel("Spinta [N]")
+            sep_axThrust_z.set_title("Spinta in funzione della quota - %s"%(rocketName))
+            sep_axThrust_z.set_yscale('log')
+            sep_axThrust_z.grid(True, which="both")
 
-            ###ISP-Z
-            sep_figIsp_z, sep_axIsp_z = plt.subplots(figsize=(8, 8),num="Impulso specifico in funzione della quota- %s"%(rocketName))
+            ###SEP ISP-Z
+            sep_figIsp_z, sep_axIsp_z = plt.subplots(figsize=(12, 8),num="Impulso specifico in funzione della quota- %s"%(rocketName))
             sep_axIsp_z.set_xlabel("Quota [m]")
             sep_axIsp_z.set_ylabel("Impulso specifico [s]")
+            sep_axIsp_z.set_title("Impulso specifico in funzione della quota - %s"%(rocketName))
+            sep_axIsp_z.set_yscale('linear')
+            sep_axIsp_z.grid(True, which="both")
+
+            ###SEP CT-Z
+            sep_figCt_z, sep_axCt_z = plt.subplots(figsize=(12, 8),num="Coefficiente di spinta in funzione della quota- %s"%(rocketName))
+            sep_axCt_z.set_xlabel("Quota [m]")
+            sep_axCt_z.set_ylabel("Coefficiente di spinta [-]")
+            sep_axCt_z.set_title("Coefficiente di spinta in funzione della quota- %s"%(rocketName))
+            sep_axCt_z.set_yscale('linear')
+            sep_axCt_z.grid(True, which="both")
 
             for blockIndex in range(len(rocket)):
                 block=rocket[blockIndex]
                 blockSolved=block['solvedData']
 
                 ###THRUST-Z
-                sep_axThrust_z.plot(blockSolved['z'].getValue(),blockSolved['thr_var'].getValue()*block['qty'],label=block['name'])
-                sep_axThrust_z.legend()
-                sep_axThrust_z.set_yscale('log')
-                sep_axThrust_z.grid(True, which="both")
-                sep_axThrust_z.set_title("Contributo spinta dei vari stadi - "+rocketName)
+                sep_axThrust_z.plot(blockSolved['z'].getValue(),blockSolved['thr_var'].getValue()*block['qty'],label=block['name'],color=self.stageColors[blockIndex])
+                axThrust_z.plot(blockSolved['z'].getValue(),blockSolved['thr_var'].getValue()*block['qty'],label="%s - %s"%(rocketName,block['name']),color=self.rocketColors[i],linestyle=self.stageStyles[blockIndex])
 
-                ###THRUST-Z
-                sep_axIsp_z.plot(blockSolved['z'].getValue(),blockSolved['I_s'].getValue(),label=block['name'])
-                sep_axIsp_z.legend()
-                sep_axIsp_z.set_yscale('linear')
-                sep_axIsp_z.grid(True, which="both")
-                sep_axIsp_z.set_title("Impulso specifico dei vari stadi - "+rocketName)
+                ###ISP-Z
+                sep_axIsp_z.plot(blockSolved['z'].getValue(),blockSolved['I_s'].getValue(),label=block['name'],color=self.stageColors[blockIndex])
+                axIsp_z.plot(blockSolved['z'].getValue(),blockSolved['I_s'].getValue(),label="%s - %s"%(rocketName,block['name']),color=self.rocketColors[i],linestyle=self.stageStyles[blockIndex])
+
+                if block['type']=='liquid':
+                    ###CT-Z
+                    sep_axCt_z.plot(blockSolved['z'].getValue(),blockSolved['ct_var'].getValue(),label=block['name'],color=self.stageColors[blockIndex])
+                    axCt_z.plot(blockSolved['z'].getValue(),blockSolved['ct_var'].getValue(),label="%s - %s"%(rocketName,block['name']),color=self.rocketColors[i],linestyle=self.stageStyles[blockIndex])
+
+            sep_axThrust_z.legend()
+            sep_figThrust_z.savefig('out/performance/single/'+("Spinta-Quota %s"%(rocketName)).replace(" ","_")+".png",dpi=sep_figThrust_z.dpi)
+
+            sep_axIsp_z.legend()
+            sep_figIsp_z.savefig('out/performance/single/'+("Isp-Quota %s"%(rocketName)).replace(" ","_")+".png",dpi=sep_figIsp_z.dpi)
+
+            sep_axCt_z.legend()
+            sep_figCt_z.savefig('out/performance/single/'+("Ct-Quota %s"%(rocketName)).replace(" ","_")+".png",dpi=sep_figCt_z.dpi)
+
+            i+=1
+
+        axThrust_z.legend()
+        figThrust_z.savefig('out/performance/multi/'+"Spinta-Quota.png",dpi=figThrust_z.dpi)
+
+        axIsp_z.legend()
+        figIsp_z.savefig('out/performance/multi/'+"Isp-Quota.png",dpi=figIsp_z.dpi)
+
+        axCt_z.legend()
+        figCt_z.savefig('out/performance/multi/'+"Ct-Quota.png",dpi=figCt_z.dpi)
