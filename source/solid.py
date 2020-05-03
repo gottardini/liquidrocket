25ba5f2f62447e7de39f19293679e8ab5acb640c
gottardini <danielegott98@gmail.com>
2020-05-03 16:06:37 +0200

    Implemented variable thrust in solid model

--- source/solid.py
+++ source/solid.py
@@ -5,7 +5,8 @@ import numpy as np
 ###TODO
 prop={
     'thr_n':UnknownVariable("Spinta nominale",'N',CalcFunction(kiloNewtonToNewton,'T_n')),
-    'thr_var':UnknownVariable("Spinta in funzione della quota",'N',CalcFunction(lambda z,t: np.full(len(z),t), 'z','thr_n')),
+    'm_p':UnknownVariable("Portata massica",'kg/s',CalcFunction(lambda t,i_s,g : t/(i_s*g),'thr_n','Is_vac','g_0')),
+    'thr_var':UnknownVariable("Spinta in funzione della quota",'N',CalcFunction(lambda i_s,g,m : i_s*g*m, 'I_s','g_0','m_p')),
     'I_s':UnknownVariable("Impulso specifico",'s',CalcFunction(lambda issl,isvac,pssl,p: issl+(isvac-issl)/pssl*(pssl-p),'Is_sl','Is_vac','p_0','p')),
 }
 
